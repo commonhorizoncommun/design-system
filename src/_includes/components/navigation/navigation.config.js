14f33e5730bd128df7de7258737a2c113316bf92
Ned Zimmerman <ned@bight.dev>
2024-02-25 10:05:42 -0400

    fix: resolve issues with navigation link styles (fix #12)

--- src/_includes/components/navigation/navigation.config.js
+++ src/_includes/components/navigation/navigation.config.js
@@ -1,24 +1,25 @@
 module.exports = {
-    title: "Navigation",
-    context: {
-        items: [
-            {
-                title: "Who We Are",
-                current: true
-            },
-            {
-                title: "How We Win"
-            },
-            {
-                title: "Campaigns"
-            },
-            {
-                title: "People’s Agenda"
-            },
-            {
-                title: "Take Action"
-            }
-        ]
-    },
-    variants: []
+  title: 'Navigation',
+  preview: 'no-padding',
+  context: {
+    items: [
+      {
+        title: 'Who We Are',
+        current: true
+      },
+      {
+        title: 'How We Win'
+      },
+      {
+        title: 'Campaigns'
+      },
+      {
+        title: 'People’s Agenda'
+      },
+      {
+        title: 'Take Action'
+      }
+    ]
+  },
+  variants: []
 };
