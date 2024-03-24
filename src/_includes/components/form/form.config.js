0b908258cab0dc09a639d9b8e015f6a84ff066fe
Ned Zimmerman <ned@bight.dev>
2024-03-24 12:05:57 -0300

    feat: finish forms implementation

---
+++ src/_includes/components/form/form.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'Form',
+  preview: 'no-padding',
+  context: {
+    id: 'common-horizon-email-en'
+  },
+  variants: [
+    {
+      title: 'Membership Form',
+      context: {
+        id: 'common-horizon-member-en'
+      }
+    }
+  ]
+};
