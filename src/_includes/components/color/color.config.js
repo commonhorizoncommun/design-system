3337434d0c2faf68881eea1b0d4d09c355c00cd4
Ned Zimmerman <ned@bight.dev>
2024-04-07 11:22:11 -0300

    feat: make colours responsive to background, replace sizes with vars
    Resolves #31.

--- src/_includes/components/color/color.config.js
+++ src/_includes/components/color/color.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   title: 'Color',
   context: {
-    colors: [
-      'cream-50',
-      'cream-100',
-      'orange-500',
-      'orange-700',
-      'purple-100',
-      'purple-650',
-      'purple-700'
-    ],
-    gradients: [
-      'cream-150-to-purple-100',
-      'purple-50-to-cream-150',
-      'purple-50-to-cream-50'
-    ]
+    colors: {
+      'cream-50': 'Cream 50',
+      'cream-100': 'Cream 100',
+      'orange-500': 'Orange 500',
+      'orange-700': 'Orange 700',
+      'purple-100': 'Purple 100',
+      'purple-650': 'Purple 650',
+      'purple-700': 'Purple 700'
+    },
+    gradients: {
+      'cream-150-to-purple-100': 'Cream 150 to Purple 100',
+      'purple-50-to-cream-150': 'Purple 50 to Cream 150',
+      'purple-50-to-cream-50': 'Purple 50 to Cream 50'
+    }
   }
 };
