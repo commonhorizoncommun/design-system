c0c3a6412762167ca0fdfefca74c3aeed7277d62
Ned Zimmerman <ned@bight.dev>
2024-04-07 21:56:07 -0300

    feat: finish page navigation styles

--- src/_includes/components/block/block.config.js
+++ src/_includes/components/block/block.config.js
@@ -24,6 +24,14 @@ module.exports = {
         content:
           '<figure data-ratio="auto"><img class="filter-orange-500" src="../../../static/images/march.jpg" alt=""></figure>'
       }
+    },
+    {
+      title: 'Page Navigation',
+      context: {
+        type: 'page-navigation',
+        content:
+          "<nav class='toc' aria-label='Table of contents'><ul role='list'><li><a href='#events'>Events</a></li><li><a href='#find-your-local-chapter'>Chapters</a></li><li><a href='#become-a-member'>Become a member</a></li><li><a href='#donate'>Donate</a></li></ul></nav>"
+      }
     }
   ]
 };
